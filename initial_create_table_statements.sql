b1b1b1dab57b49fa04e750a279965baafea6687c
mertbarutcuoglu <kbarutcu@students.cs.ubc.ca>
2021-10-25 18:52:42 -0700

    Adds create table statements.

---
+++ initial_create_table_statements.sql
@@ -0,0 +1,52 @@
+CREATE TABLE Race(
+    RaceName CHAR(75),
+    Laps INTEGER,
+    EndDate DATE,
+    FastestLapAverageSpeed FLOAT NOT NULL,
+    CircuitName CHAR(75) NOT NULL,
+    PRIMARY KEY(RaceName)
+    FOREIGN KEY(EndDate)
+        REFERENCES RaceDate
+        ON UPDATE CASCADE
+    FOREIGN KEY(FastestLapAverageSpeed)
+        REFERENCES FastestLap
+        ON DELETE CASCADE
+    FOREIGN KEY(CircuitName)
+        REFERENCES Circuit
+        ON DELETE CASCADE
+)
+ 
+CREATE TABLE RaceDate(
+    StartDate DATE,
+    EndDate DATE,
+    PRIMARY KEY(EndDate)
+)
+
+CREATE TABLE Circuit (
+    CircuitName CHAR(75),
+    Length INTEGER,
+    City CHAR(30) NOT NULL,
+    PRIMARY KEY(CircuitName)
+    FOREIGN KEY(City) REFERENCES CircuitLocation 
+        ON DELETE CASCADE
+)
+CREATE TABLE CircuitLocation (
+    City CHAR(30),
+    Country CHAR(30)
+    PRIMARY key(City) 
+)
+
+CREATE TABLE FastestLap (
+    AverageSpeed FLOAT,
+    LapTime TIME,
+    RaceName CHAR(75) NOT NULL,
+    DriverNumber INTEGER NOT NULL,
+    PRIMARY KEY (AverageSpeed, RaceName),
+    FOREIGN KEY (DriverNumber) REFERENCES Driver
+        ON DELETE CASCADE
+        ON UPDATE CASCADE 
+    FOREIGN KEY (RaceName) 
+        REFERENCES Race
+        ON DELETE CASCADE
+        ON UPDATE CASCADE 
+)
